f84abffbd60549937874764f1631ec3c51e82b05
ddoubledamage <47157358+ddoubledamage@users.noreply.github.com>
2025-02-07 15:15:13 +0300

    patch

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
-console.log('Happy developing ✨')
+const Koa = require('koa');
+const slow = require('koa-slow');
+const cors = require('koa-cors');
+const app = new Koa();
+const PORT = 3000;
+
+app.use(cors());
+
+//app.use(slow({ delay: 3000 }));
+
+
+app.use(async (ctx) => {
+    ctx.body = "Hello, Koa with delay!";
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
